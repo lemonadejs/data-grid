f1b74c7469b444ebb71ad1cd0d1173c2eae76969
Nícolas Jessé <nicolasjesseam@gmail.com>
2023-05-04 09:06:58 -0300

    add event test case

---
+++ test/events.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+
+describe('Create a Datagrid and trigger event', () => {
+    it('onupdate', () => {
+        let stateOutside = 'before onupdate'
+        let dg = Datagrid(root, {
+            data: [
+                { date: '2022-01-01' },
+                { date: '2022-01-02' },
+                { date: '2022-01-03' },
+                { date: '2022-01-04' },
+                { date: '2022-01-05' },
+                { date: '2022-02-06' },
+                { date: '2022-02-07' },
+            ],
+            columns: [
+                { name: 'date', headerName: 'Date' },
+            ],
+            search: true,
+            onupdate: function() {
+                stateOutside = 'after onupdate'
+            }
+        })
+        
+        dg.lemon.self.setValue(0, 0, 'Something')
+        expect(stateOutside).to.equal('after onupdate')
+    })
+});
